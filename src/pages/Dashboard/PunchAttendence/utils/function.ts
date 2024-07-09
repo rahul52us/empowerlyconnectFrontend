7bcf27c713bb85e2f2e7a9a8094e6f092af6c568
rahul-sequelstring <rahul.kushwah@sequelstring.com>
2024-07-09 08:40:31 +0530

    changes

--- src/pages/Dashboard/PunchAttendence/utils/function.ts
+++ src/pages/Dashboard/PunchAttendence/utils/function.ts
@@ -1,11 +1,13 @@
-import { convertToReadableIST } from "../../../../config/constant/dateUtils";
+import { convertToReadableIST, formatDate } from "../../../../config/constant/dateUtils";
 
 export const generateResponse = (data: any) => {
   return data.map((item: any) => {
+    const date = formatDate(item.data)
     const punchInTimeReadable = convertToReadableIST(item.punchInTime);
     const punchOutTimeReadable = convertToReadableIST(item.punchOutTime);
     return {
       ...item,
+      date:date,
       punchInTime:punchInTimeReadable,
       punchOutTime:punchOutTimeReadable,
     };
