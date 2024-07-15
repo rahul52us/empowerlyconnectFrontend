6b436496f03c7bb63afd16df98e0c98f6cbeed9c
rahul-sequelstring <rahul.kushwah@sequelstring.com>
2024-07-15 22:00:35 +0530

    apply api of profile data

--- src/pages/main/profile/ProfileIndex.tsx
+++ src/pages/main/profile/ProfileIndex.tsx
@@ -1,90 +1,32 @@
-import { useState, useEffect } from "react";
-import { observer } from "mobx-react-lite";
-import ProfileContainer from "../../../config/component/profile/ProfileContainer";
-import store from "../../../store/store";
-import { studentInitialValues } from "../../../config/component/profile/utils/constant";
-import { studentEditValidation } from "../../../config/component/profile/utils/validation";
-import { EditStudentSideTab } from "../../Dashboard/UserTypes/Student/utils/constant";
-import { currentYear, oneYearLater } from "../../../config/constant/dateUtils";
-import { main } from "../../../config/constant/routes";
+import { observer } from "mobx-react-lite"
+import store from "../../../store/store"
+import { useEffect, useState } from "react"
+import SpinnerLoader from "../../../config/component/Loader/SpinnerLoader"
 
 const ProfileIndex = observer(() => {
-  const {
-    classStore: { getClasses, classes },
-    auth: { openNotification, changePasswordStore, user, updateProfile },
-  } = store;
-
-  const date = useState({
-    startYear: currentYear,
-    endYear: oneYearLater,
-  })[0];
+  const [loading,setLoading] = useState(false)
+  const [userDetails,setUserDetails] = useState(null)
+  const {Employe : {getEmployesDetailsById}, auth : {user}} = store
 
   useEffect(() => {
-    if (user) {
-      getClasses({ startYear: date.startYear, endYear: date.endYear })
-        .then((data) => {
-          console.log(data);
-        })
-        .catch((err) => {
-          openNotification({
-            title: "Failed to Get Classes",
-            message: err.message,
-            type: "error",
-          });
-        });
+    if(user){
+      setLoading(true)
+      getEmployesDetailsById(user._id).then((data) => {
+        setUserDetails(data)
+      }).catch(() => {
+      }).finally(() => {
+        setLoading(false)
+      })
     }
-  }, [getClasses, openNotification, date, user]);
+  },[getEmployesDetailsById,user])
 
-  const handleSubmitProfile = (
-    values: any,
-    setSubmitting: any,
-    resetForm: any,
-    setErrors: any,
-    setShowError: any
-  ) => {
-    try {
-      updateProfile(values)
-        .then((data) => {
-          openNotification({
-            title: "UPDATE SUCCESSFULLY",
-            message: data.message,
-          });
-          setErrors({});
-          setShowError(false);
-          console.log(resetForm);
-        })
-        .catch((err) => {
-          openNotification({
-            title: "Failed to Update Profile",
-            message: err.message,
-            type: "error",
-          });
-        })
-        .finally(() => {
-          setSubmitting(false);
-        });
-    } catch (err: any) {
-      openNotification({
-        title: "FAILED TO UPDATE",
-        message: err.message,
-        type: "error",
-      });
-    }
-  };
+  console.log('the user details', userDetails)
 
-  return user ? (
-    <ProfileContainer
-      initialValues={studentInitialValues(user)}
-      profileData={studentInitialValues(user)}
-      validations={studentEditValidation}
-      changePassword={changePasswordStore}
-      classes={classes.data}
-      sideTab={EditStudentSideTab}
-      editTabLink={`${main.profile}?`}
-      handleSubmitProfile={handleSubmitProfile}
-      type="edit"
-    />
-  ) : null;
-});
+  return (
+    loading ?
+    <SpinnerLoader /> :
+    <div>ProfileIndex</div>
+  )
+})
 
-export default ProfileIndex;
+export default ProfileIndex
