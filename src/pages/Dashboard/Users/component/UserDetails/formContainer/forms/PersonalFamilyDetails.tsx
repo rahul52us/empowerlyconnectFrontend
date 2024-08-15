2a5318b539b10959eac5a2e9d2ba97ef49564a6b
rahul-sequelstring <rahul.kushwah@sequelstring.com>
2024-08-15 19:56:07 +0530

    mior cahnegs

--- src/pages/Dashboard/Users/component/UserDetails/formContainer/forms/PersonalFamilyDetails.tsx
+++ src/pages/Dashboard/Users/component/UserDetails/formContainer/forms/PersonalFamilyDetails.tsx
@@ -1,5 +1,5 @@
 import { Formik, Form, FieldArray } from "formik";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import {
   Box,
   Heading,
@@ -13,6 +13,7 @@ import {
 import CustomInput from "../../../../../../../config/component/CustomInput/CustomInput";
 import CustomSubmitBtn from "../../../../../../../config/component/CustomSubmitBtn/CustomSubmitBtn";
 import FormModel from "../../../../../../../config/component/common/FormModel/FormModel";
+import NotFoundData from "../../../../../../../config/component/NotFound/NotFoundData";
 
 const FamilyDetails = ({
   handleSubmitProfile,
@@ -23,6 +24,17 @@ const FamilyDetails = ({
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [editIndex, setEditIndex] = useState<number | null>(null);
   const [collapsedIndex, setCollapsedIndex] = useState<number | null>(null);
+  const [showNotFound, setShowNotFound] = useState<boolean>(true);
+
+  useEffect(() => {
+    // Update the visibility of NotFoundData when relations change
+    // setShowNotFound(initialValues.relations?.length === 0);
+  }, [initialValues]);
+
+  const handleAddRelation = () => {
+    setEditIndex(null);
+    onOpen();
+  };
 
   return (
     <Box p={6} borderRadius="lg" boxShadow="md">
@@ -30,6 +42,15 @@ const FamilyDetails = ({
         Family Details
       </Heading>
       <Divider mb={8} />
+
+      {/* Conditionally render NotFoundData component */}
+        {showNotFound && <NotFoundData
+          onClick={handleAddRelation}
+          btnText="Add Member"
+          title="No Members Found"
+          subTitle="Start by creating a new member."
+        />}
+
       <Formik
         validationSchema={validations}
         initialValues={initialValues}
@@ -48,108 +69,129 @@ const FamilyDetails = ({
             <FieldArray name="relations">
               {({ push, remove }) => (
                 <>
-                  <Box mb={8}>
-                    {values.relations.map((relation: any, index: number) => (
-                      <Box
-                        key={index}
-                        mb={6}
-                        borderWidth="1px"
-                        borderRadius="md"
-                        boxShadow="sm"
-                        transition="background 0.3s ease"
-                      >
-                        <Flex
-                          alignItems="center"
-                          justifyContent="space-between"
-                          // mb={4}
-                          p={4}
-                          flexDirection={{base : 'column', md : 'row'}}
-                          rowGap={4}
-                          maxW="80vw"
+                  {/* Conditionally render relations section */}
+                  {!showNotFound && (
+                    <Box mb={8}>
+                      {values.relations.map((relation: any, index: number) => (
+                        <Box
+                          key={index}
+                          mb={6}
+                          borderWidth="1px"
+                          borderRadius="md"
+                          boxShadow="sm"
+                          transition="background 0.3s ease"
+                          _hover={{ bg: "gray.50" }}
                         >
-                          <Box fontSize="lg" fontWeight="bold">
-                            Family Member {index + 1}
-                          </Box>
-                          <Flex columnGap={4}>
-                            <Button
-                              colorScheme="teal"
-                              variant="outline"
-                              borderRadius="full"
-                              p={3}
-                              _hover={{ bg: "teal.100" }}
-                              onClick={() => {
-                                setEditIndex(index);
-                                onOpen();
-                              }}
-                            >
-                              Edit
-                            </Button>
-                            <Button
-                              colorScheme="red"
-                              variant="outline"
-                              borderRadius="full"
-                              p={3}
-                              _hover={{ bg: "red.100" }}
-                              onClick={() => remove(index)}
-                            >
-                              Remove
-                            </Button>
-                            <Button
-                              borderRadius="full"
-                              p={3}
-                              _hover={{ bg: "blue.100" }}
-                              onClick={() =>
-                                setCollapsedIndex(
-                                  collapsedIndex === index ? null : index
-                                )
-                              }
-                            >
-                              {collapsedIndex === index ? "Hide Details" : "Show Details"}
-                            </Button>
+                          <Flex
+                            alignItems="center"
+                            justifyContent="space-between"
+                            p={4}
+                            flexDirection={{ base: "column", md: "row" }}
+                            rowGap={4}
+                            maxW="80vw"
+                          >
+                            <Box fontSize="lg" fontWeight="bold">
+                              Family Member {index + 1}
+                            </Box>
+                            <Flex columnGap={4}>
+                              <Button
+                                colorScheme="teal"
+                                variant="outline"
+                                borderRadius="full"
+                                p={3}
+                                _hover={{ bg: "teal.100" }}
+                                onClick={() => {
+                                  setEditIndex(index);
+                                  onOpen();
+                                }}
+                              >
+                                Edit
+                              </Button>
+                              <Button
+                                colorScheme="red"
+                                variant="outline"
+                                borderRadius="full"
+                                p={3}
+                                _hover={{ bg: "red.100" }}
+                                onClick={() => {
+                                  remove(index);
+                                  // Update NotFoundData visibility after removal
+                                  if (values.relations.length === 0) {
+                                    setShowNotFound(true);
+                                  }
+                                }}
+                              >
+                                Remove
+                              </Button>
+                              <Button
+                                borderRadius="full"
+                                p={3}
+                                _hover={{ bg: "blue.100" }}
+                                onClick={() =>
+                                  setCollapsedIndex(
+                                    collapsedIndex === index ? null : index
+                                  )
+                                }
+                              >
+                                {collapsedIndex === index
+                                  ? "Hide Details"
+                                  : "Show Details"}
+                              </Button>
+                            </Flex>
                           </Flex>
-                        </Flex>
-                        <Collapse in={collapsedIndex === index}>
-                          <Box mt={4} p={2}>
-                            <Grid
-                              gridTemplateColumns={{ base: "1fr", md: "1fr 1fr" }}
-                              gap={4}
-                            >
-                              {Object.entries(relation).map(([field, value]) => (
-                                <CustomInput
-                                  key={field}
-                                  name={`relations.${index}.${field}`}
-                                  label={capitalize(field.replace("_", " "))}
-                                  type="text"
-                                  placeholder={capitalize(field.replace("_", " "))}
-                                  value={value}
-                                  showError={showError}
-                                />
-                              ))}
-                            </Grid>
-                          </Box>
-                        </Collapse>
-                      </Box>
-                    ))}
-                    <Button
-                      mt={6}
-                      colorScheme="teal"
-                      variant="solid"
-                      borderRadius="full"
-                      p={4}
-                      _hover={{ bg: "teal.600" }}
-                      onClick={() => {
-                        setEditIndex(null);
-                        onOpen();
-                      }}
-                    >
-                      Add Relation
-                    </Button>
-                  </Box>
+                          <Collapse in={collapsedIndex === index}>
+                            <Box mt={4} p={2}>
+                              <Grid
+                                gridTemplateColumns={{
+                                  base: "1fr",
+                                  md: "1fr 1fr",
+                                }}
+                                gap={4}
+                              >
+                                {Object.entries(relation).map(
+                                  ([field, value]) => (
+                                    <CustomInput
+                                      key={field}
+                                      name={`relations.${index}.${field}`}
+                                      label={capitalize(field.replace("_", " "))}
+                                      type="text"
+                                      placeholder={capitalize(
+                                        field.replace("_", " ")
+                                      )}
+                                      value={value}
+                                      showError={showError}
+                                    />
+                                  )
+                                )}
+                              </Grid>
+                            </Box>
+                          </Collapse>
+                        </Box>
+                      ))}
+                      <Button
+                        mt={6}
+                        colorScheme="teal"
+                        variant="solid"
+                        borderRadius="full"
+                        p={4}
+                        _hover={{ bg: "teal.600" }}
+                        onClick={handleAddRelation}
+                      >
+                        Add Relation
+                      </Button>
+                    </Box>
+                  )}
+
+                  {/* Conditionally render FormModel */}
                   <FormModel
                     footer={false}
                     isOpen={isOpen}
                     close={onClose}
-                    title={editIndex !== null ? "Edit Relation" : "Add Relation"}
+                    title={
+                      editIndex !== null
+                        ? "Edit Relation"
+                        : "Add Relation"
+                    }
                   >
                     <Formik
                       initialValues={
@@ -177,6 +219,7 @@ const FamilyDetails = ({
                         } else {
                           push(formValues);
                         }
+                        // Update NotFoundData visibility after saving
                         onClose();
                         setSubmitting(false);
                       }}
@@ -185,7 +228,10 @@ const FamilyDetails = ({
                         <Form>
                           <Box mb={4} p={4}>
                             <Grid
-                              gridTemplateColumns={{ base: "1fr", md: "1fr 1fr" }}
+                              gridTemplateColumns={{
+                                base: "1fr",
+                                md: "1fr 1fr",
+                              }}
                               gap={4}
                             >
                               {[
@@ -207,7 +253,9 @@ const FamilyDetails = ({
                                   name={field}
                                   label={capitalize(field.replace("_", " "))}
                                   type="text"
-                                  placeholder={capitalize(field.replace("_", " "))}
+                                  placeholder={capitalize(
+                                    field.replace("_", " ")
+                                  )}
                                   value={values[field]}
                                   error={errors[field]}
                                   showError={showError}
@@ -224,6 +272,7 @@ const FamilyDetails = ({
                                 _hover={{ bg: "teal.600" }}
                                 type="submit"
                                 isLoading={isSubmitting}
+                                onClick={() => setShowNotFound(false)}
                               >
                                 Save
                               </Button>
