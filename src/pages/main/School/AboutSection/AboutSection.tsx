6c0e1494ab5a35ca72750c64545bad7519b40f0c
Vivek5446 <uknownwarrior04@gmailcom>
2024-10-15 23:03:16 +0530

    stastic counter component

--- src/pages/main/School/AboutSection/AboutSection.tsx
+++ src/pages/main/School/AboutSection/AboutSection.tsx
@@ -1,10 +1,18 @@
-import { Box, Container, Heading, Text, Flex, Image } from "@chakra-ui/react";
+import {
+  Box,
+  Container,
+  Heading,
+  Text,
+  Flex,
+  Image,
+  VStack,
+} from "@chakra-ui/react";
 
 export default function AboutSection() {
   return (
     <Box py={16} bg="white">
       <Container maxW="container.xl">
-        <Heading as="h2" size="xl" textAlign="center" mb={8}>
+        <Heading as="h2" size="xl" textAlign="center" mb={10}>
           About Our School
         </Heading>
         <Flex direction={{ base: "column", md: "row" }} gap={8} align="center">
@@ -14,20 +22,33 @@ export default function AboutSection() {
               alt="School campus"
               w={520}
               rounded={12}
-              h={400}
+              h={380}
               objectFit={"cover"}
             />
           </Box>
           <Box flex="1">
-            <Text mb={4} color="gray.700" fontSize={"xl"}>
-              Evergreen Academy is a prestigious institution dedicated to
-              providing high-quality education to students from diverse
-              backgrounds.
-            </Text>
-            <Text color="gray.700" fontSize={"lg"}>
-              Founded in 1995, we have a rich history of academic excellence and
-              have produced numerous successful alumni.
-            </Text>
+            <VStack color="gray.700" fontSize={"lg"} spacing={6}>
+              <Text>
+                Evergreen Academy is a prestigious institution dedicated to
+                providing high-quality education to students from diverse
+                backgrounds.
+              </Text>
+              <Text>
+                Our dedicated faculty consists of passionate educators with
+                extensive expertise in their respective fields.
+              </Text>
+              <Text>
+                Founded in 1995, we have a rich history of academic excellence
+                and have produced numerous successful alumni.
+              </Text>
+              <Text>
+                As we look to the future, Evergreen Academy remains committed to
+                upholding its mission of academic excellence and innovation. We
+                continually invest in state-of-the-art facilities and resources,
+                ensuring that our students have access to the tools necessary
+                for success .
+              </Text>
+            </VStack>
           </Box>
         </Flex>
       </Container>
